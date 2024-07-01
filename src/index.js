9fa9f6c9fcf61f371cdc424df219e9906132af62
Aleksa Cekić <aleksa.cekic8@gmail.com>
2024-07-01 23:37:49 +0200

    initial

---
+++ src/index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const { Client, IntentsBitField } = require("discord.js");
+
+const token = process.env.TOKEN;
+const channelId = process.env.CHANNEL_ID;
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", (c) => {
+  console.log(`The bot [${c.user.username}] is online`);
+
+  const channel = client.channels.cache.get(channelId);
+
+  if (channel) {
+    const today = new Date();
+    const day = today.getDate();
+
+    if (day === 22) {
+      channel
+        .send("@everyone Podsetnik za spotify subsripciju!")
+        .then(() => console.log("Message sent successfully"))
+        .catch((error) => console.error("Failed to send message:", error));
+    }
+  }
+});
+
+client.login(token).catch((error) => console.log("Failed to login:", error));
